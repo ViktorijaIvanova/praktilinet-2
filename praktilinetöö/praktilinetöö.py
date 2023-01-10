4031e83eea22ff8ad0d28a0e2cbb76ef9b8e0ceb
opilane <opilane@HP-CZC2349HT1>
2023-01-10 09:57:00 +0200

    Add project files.

---
+++ praktilinetöö/praktilinetöö.py
@@ -0,0 +1,20 @@
+from math import*
+from random import*
+a = 1
+while a < 101:
+    print("Tsükkel lõpetatud", a, "aeg(a)")
+    a = a+1
+print("tsükkel on lõpp")
+
+
+#задача я хожу в тех пока не наступают выходные через while or for
+
+
+
+
+
+
+
+
+
+
